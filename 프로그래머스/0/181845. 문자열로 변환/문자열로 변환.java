6677e0a1df07c7711bdf5e595366c67aa2c0a7ff
0biin0 <154484798+0biin0@users.noreply.github.com>
2024-07-23 19:14:15 +0900

    [level 0] Title: 문자열로 변환, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        answer = Integer.toString(n);
+        return answer;
+    }
+}
+// 자바에서 정수를 문자열로 변환하는 방법 : Integer.toString()
