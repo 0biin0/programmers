6dd6726b817ae6d6c507da0e5d6c2c054fe3ca3b
0biin0 <154484798+0biin0@users.noreply.github.com>
2024-07-26 19:22:21 +0900

    [level 0] Title: 배열 만들기 4, Time: 26.14 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+       ArrayList<Integer> stk = new ArrayList<Integer>();
+        for(int i = 0; i<arr.length; i++){
+            if(stk.size() == 0){
+                stk.add(arr[i]);
+            }else {
+                if(stk.get(stk.size()-1) < arr[i]){
+                    stk.add(arr[i]);
+                }else {  
+                    stk.remove(stk.size()-1);
+                    i--;
+                }
+                
+            }
+                  
+        }
+        int[] answer = new int[stk.size()];
+        for(int i = 0; i< answer.length; i++){
+            answer[i] = stk.get(i);
+        }
+        return answer;
+    }
+}
