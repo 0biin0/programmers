bbcd1eac45ddf607117efcd90988700416f8b6c5
0biin0 <154484798+0biin0@users.noreply.github.com>
2024-07-29 23:31:45 +0900

    [level 0] Title: 리스트 자르기, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,7 +1,41 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
         int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        int[] answer;
+        
+        if(n == 1) {
+            answer = new int[b + 1];
+            for(int i = 0; i <= b; i++) {
+                answer[i] = num_list[i];
+            }
+        } else if(n == 2) {
+            answer = new int[num_list.length - a];
+            for(int i = a; i < num_list.length; i++) {
+                answer[i - a] = num_list[i];
+            }
+        } else if(n == 3) {
+            answer = new int[b - a + 1];
+            for(int i = a; i <= b; i++) {
+                answer[i - a] = num_list[i];
+            }
+        } else {
+            int size = (b - a) / c + 1;
+            answer = new int[size];
+            for(int i = a, j = 0; i <= b; i += c, j++) {
+                answer[j] = num_list[i];
+            }
+        }
+        
+        return answer;
+    }
+}
+
+/*
+
+int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
         ArrayList<Integer> list = new ArrayList<>();
@@ -26,7 +60,4 @@ class Solution {
         for(int i = 0; i<list.size(); i++){
             answer[i] = list.get(i);
         }
-        
-        return answer;
-    }
-}
+*/
